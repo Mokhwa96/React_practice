b3441348a2294b3edcd34e24d130672830262896
Mokhwa96 <149074033+Mokhwa96@users.noreply.github.com>
2023-10-30 11:30:38 +0900

    Delete learn-express3 directory

--- learn-express3/my_spawn.js
+++
@@ -1,8 +0,0 @@
-const spawn = require('child_process').spawn; 
-const py_process = spawn('python', ['test.py']); 
-py_process.stdout.on('data', function(data){ 
-  console.log(data.toString());
-});
-py_process.stderr.on('data', function(data){ 
-  console.log(data.toString());
-});
