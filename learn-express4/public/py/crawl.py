ba1fad8e5eaba8e966b95c2a4e13d8fe95b87f5e
Mokhwa96 <149074033+Mokhwa96@users.noreply.github.com>
2023-10-30 11:30:02 +0900

    Delete learn-express4 directory

--- learn-express4/public/py/crawl.py
+++
@@ -1,9 +0,0 @@
-import sys
-import urllib.request
-from bs4 import BeautifulSoup
-print("python version:", sys.version)
-url = "https://www.nytimes.com/"
-res = urllib.request.urlopen(url)
-webpage = res.read().decode('utf-8')
-soup = BeautifulSoup(webpage, 'html.parser')
-print(soup.find('title').text)
